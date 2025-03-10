702241e3003245ea8dbc9a697f385a09a6f5e992
Rishabh-mikku <mikkurishabh94@gmail.com>
2025-03-10 23:33:45 +0530

    Added Login Page xml and included Get Started logic in HomePage.
    Added images needed in login page to @drawable

--- app/src/main/java/com/example/greenspace/HomePage.kt
+++ app/src/main/java/com/example/greenspace/HomePage.kt
@@ -1,4 +1,31 @@
 package com.example.greenspace
 
-class HomePage {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePage : AppCompatActivity() {
+
+    private lateinit var getStartedBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        getStartedBtn = findViewById(R.id.btnGetStarted)
+        getStartedBtn.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+    }
 }
